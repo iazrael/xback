d8931342fbf0b16d55ec663c73e3fb21b4eb6f97
azrael <azrael@imatlas.com>
2013-02-04 17:03:53 +0800

    init
    Signed-off-by: azrael <azrael@imatlas.com>

---
+++ README.md
@@ -0,0 +1,17 @@
+如果你做过 Android 的 Web 开发, 那么你应该发现了页面是无法监听屏幕右下方(也可能是左下方)的返回按钮的, 使用 XBack 则可以帮你粗略的监听页面返回事件(虽然不太准, ^_^)
+
+原理
+====
+XBack 使用 HTML5 的 History 新 API pushState 来曲线监听 Android 设备的返回按钮, 在页面加载后往历史记录写入一个 state, 并监听 popstate 事件, 如果返回按钮被按下, 则会触发 popstate, 比对到触发的 state 跟写入的一致, 则认为返回按钮被按下了.
+
+Example
+========
+页面引入xback.js后, 使用下面的方法监听返回按钮事件, 支持多方法绑定
+
+XBack.listen(function(){
+	alert('oh! you press the back button');
+});
+
+XBack.listen(function(){
+	alert('ah, press press press');
+});
---
+++ xback.js
@@ -0,0 +1,40 @@
+/**
+ * 使用 HTML5 的 History 新 API pushState 来曲线监听 Android 设备的返回按钮
+ * @author azrael  
+ * @date 2013/02/04
+ * @version 1.0
+ * @example
+ * XBack.listen(function(){
+		alert('oh! you press the back button');
+	});
+ */
+;!function(pkg, undefined){
+	var STATE = 'back';
+	var element;
+
+	var onPopState = function(event){
+		event.state === STATE && fire();
+	}
+
+	var record = function(state){
+		history.pushState(state, null, location.href);
+	}
+
+	var fire = function(){
+		var event = document.createEvent('Events');
+		event.initEvent(STATE, false, false);
+		element.dispatchEvent(event);
+	}
+
+	var listen = function(listener){
+		element.addEventListener(STATE, listener);
+	}
+
+	!function(context){
+		element = document.createElement('span');
+		window.addEventListener('popstate', onPopState);
+		this.listen = listen;
+		record(STATE);
+	}.call(window[pkg] = window[pkg] || {});
+
+}('XBack');
